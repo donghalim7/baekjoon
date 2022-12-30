9881153112ca52016e41146f1ffa882300ae1fb9
donghalim7 <donghalim7@gmail.com>
2022-12-30 16:37:04 +0900

    _

--- 10989_ex.py
+++ 10989_ex.py
@@ -11,3 +11,16 @@
 #     print(i)
 
 #for문 속에서 append를 사용하면 메모리 재할당이 이루어져 메모리를 효율적으로 사용하지 못함
+
+import sys
+
+n = int(sys.stdin.readline())
+num_list = [0] * 10001
+
+for _ in range(n):
+    num_list[int(sys.stdin.readline())] += 1
+
+for i in range(10001):
+    if num_list[i] != 0:
+        for j in range(num_list[i]):
+            print(i)
