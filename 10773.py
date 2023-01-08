b7ba416cc89be693d38e17c975f79c8e7b1a8703
donghalim7 <donghalim7@gmail.com>
2023-01-08 13:56:10 +0900

    _

---
+++ 10773.py
@@ -0,0 +1,15 @@
+import sys
+
+K = int(sys.stdin.readline())
+
+stk = []
+
+for _ in range(K):
+    num = int(sys.stdin.readline())
+
+    if num == 0:
+        stk.pop()
+    else:
+        stk.append(num)
+
+print(sum(stk))
