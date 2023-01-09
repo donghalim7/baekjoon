ae0d05efbce6efaad65f8270eebc543e5a179b9d
donghalim7 <donghalim7@gmail.com>
2023-01-10 03:12:05 +0900

    _

---
+++ 11723.py
@@ -0,0 +1,37 @@
+import sys
+
+m = int(sys.stdin.readline())
+s = set()
+
+
+for _ in range(m):
+    cmd = sys.stdin.readline().split()
+    if len(cmd) != 1:
+        cmd[1] = int(cmd[1])
+
+
+
+    if cmd[0] == "add":
+        s.add(cmd[1])
+        
+    elif cmd[0] == "remove":
+        s.remove(cmd[1])
+    
+    elif cmd[0] == "check":
+        if cmd[1] in s:
+            print(1)
+        else:
+            print(0)
+
+    elif cmd[0] == "toggle":
+        if cmd[1] in s:
+            s.remove(cmd[1])
+        else:
+            s.add(cmd[1])
+
+    elif cmd[0] == "all":
+        s.clear()
+        s.update(i for i in range(1, 21))
+
+    else:
+        s.clear()
