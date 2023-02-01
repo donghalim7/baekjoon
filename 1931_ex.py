e28b4cbb6f7fe1f840e90da46c65d34422c96770
donghalim7 <donghalim7@gmail.com>
2023-02-02 01:49:15 +0900

    _

--- 1931_ex.py
+++ 1931_ex.py
@@ -1,20 +1,14 @@
-import sys
-
-N = int(sys.stdin.readline())
-
-time = [[0]*2 for _ in range(N)]
-for i in range(N):
-    s, e = map(int, sys.stdin.readline().split())
-    time[i][0] = s
-    time[i][1] = e
-
-time.sort(key = lambda x: (x[1], x[0]))
-
-cnt = 1
-end_time = time[0][1]
-for i in range(1, N):
-    if time[i][0] >= end_time:
+n = int(input())
+s = []
+for i in range(n):
+    first, second = map(int, input().split())
+    s.append([first, second])
+s = sorted(s, key=lambda a: a[0])
+s = sorted(s, key=lambda a: a[1])
+last = 0
+cnt = 0
+for i, j in s:
+    if i >= last:
         cnt += 1
-        end_time = time[i][1]
-
+        last = j
 print(cnt)
